c3b0d8fad683caebe7512d06cbda99a7e958fa2f
Mafahes <vadm11.12@mail.ru>
2021-12-01 17:43:53 +0300

    update

--- src/app/pages/ticket-list/ticket-list.component.ts
+++ src/app/pages/ticket-list/ticket-list.component.ts
@@ -18,24 +18,40 @@ export class TicketListComponent implements OnInit, OnDestroy {
     private api: ApiService,
     private arouter: ActivatedRoute,
     private socket: SignalRService
-  ) { }
+  ) {}
   interval: any;
   queue: Queue[][] = [];
   selectedItem: any;
   selectedPage = 0;
+  audio = new Audio();
+  async playSounds(arr: Queue[]) {
+    var sounds = await this.api.getSounds().toPromise();
+    for (const i of arr.filter((e) => e.session !== null)) {
+      if(!!sounds.find((e) => e.name === i.name)) {
+        this.audio.src = sounds.find((e) => e.name === i.name).file.fullUrl;
+        this.audio.load();
+        await this.audio.play();
+      }
+    }
+  }
   async getQueue(init = false): Promise<void> {
     await new Promise((res, rej) => {
       this.arouter.paramMap.subscribe(async (e) => {
-        var a = _.chunk((
-          await this.api.getQueue().pipe(
-            map(i => i.filter(e2 => e2.roomId === parseInt(e.get('id'))))
-          ).toPromise()), 5);
-        console.log(_(this.queue).differenceWith(a, _.isEqual).isEmpty());
+        var src = await this.api.getQueue().pipe(
+          map(i => i.filter(e2 => e2.roomId === parseInt(e.get('id'))))
+        ).toPromise();
+        var a = _.chunk((src), 4);
+        if(_.differenceBy(src, _.flattenDeep(this.queue), 'session.id').length > 0) {
+          this.playSounds(_.differenceBy(src, _.flattenDeep(this.queue), 'session.id'));
+        }
+        if(!_(this.queue).differenceWith(a, _.isEqual).isEmpty()) {
+          this.selectedPage = 0;
+          this.selectedItem = 0;
+        }
         if(init) {
           this.queue = a;
         }
         if(!!this.interval && !_(this.queue).differenceWith(a, _.isEqual).isEmpty()) {
-          clearInterval(this.interval);
           this.queue = a;
         }
         if(this.queue.length === 0) {
@@ -47,34 +63,27 @@ export class TicketListComponent implements OnInit, OnDestroy {
     })
   }
   async parseData(init = false): Promise<void> {
-    clearInterval(this.interval);
     await this.getQueue(init);
     // setTimeout(() => this.router.navigate(['/safety']), 14000);
-    this.interval = setInterval(() => {
-      console.log('interval');
-      if(this.queue.length === 0) {
-        this.selectedPage = 0;
-        this.selectedItem = 0;
-        return;
-      }
-      if((this.selectedItem + 1) === this.queue[this.selectedPage].length) {
-        this.selectedPage = this.queue.length === (this.selectedPage + 1) ? 0 : this.selectedPage + 1;
-        this.selectedItem = 0;
-        return;
-      }
-      this.selectedItem = this.selectedItem === undefined ? 0 : this.selectedItem + 1 === this.queue[this.selectedPage].length ? 0 : this.queue[this.selectedPage].length < this.selectedItem + 1 ? 0 : this.selectedItem + 1;
-    }, 1000);
+    if(init) {
+      this.interval = setInterval(() => {
+        if(this.queue.length === 0) {
+          this.selectedPage = 0;
+          this.selectedItem = 0;
+          return;
+        }
+        if((this.selectedItem + 1) === this.queue[this.selectedPage].length) {
+          this.selectedPage = this.queue.length === (this.selectedPage + 1) ? 0 : this.selectedPage + 1;
+          this.selectedItem = 0;
+          return;
+        }
+        this.selectedItem = this.selectedItem === undefined ? 0 : this.selectedItem + 1 === this.queue[this.selectedPage].length ? 0 : this.queue[this.selectedPage].length < this.selectedItem + 1 ? 0 : this.selectedItem + 1;
+      }, 1000);
+    }
   }
   ngOnInit(): void {
     this.parseData(true);
     this.socket.dataTransferSub('work').subscribe(async (e: any) => {
-      var sound = (await this.api.getSounds().toPromise()).filter((e2) => e2.name === e.user.name);
-      if(sound.length > 0) {
-        let audio = new Audio();
-        audio.src = sound[0].file.fullUrl;
-        audio.load();
-        audio.play();
-      }
       this.parseData();
     });
   }
